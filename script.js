faada10057244948f60a768d68756356ba65c660
Simeon Ogunyemi <oguny006@umn.edu>
2020-04-16 18:04:08 -0500

    Started the variables for the characters for pw generator

--- script.js
+++ script.js
@@ -3,36 +3,40 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-function generatePassword () {
-var lower = ("abcdefghijklmnopqrstuvwxyz")
-var Special = []
-var numbers = ("")
+function generatePassword() {
+  var lower = ("abcdefghijklmnopqrstuvwxyz");
+  var upper = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  var special = (" !@#$%^&*()_-=+~`|}{][\/?<>;:.,'" + '"');
+  var numbers = ("0123456789");
 
 }
 
 function writePassword() {
+  var password = ("")
+
+
   alert("Welcome to Password Generator! Password will be generated based on your answers!");
   var length = prompt("How many Characters would you like? (8 - 128 Characters!)");
-  if (length >= 8 && length <=128) {
-    var upPw = confirm("Would you like an Uppercase letter?"); 
+  if (length >= 8 && length <= 128) {
+    var upPw = confirm("Would you like an Uppercase letter?");
     var lowPw = confirm("Would you like lowercase letters?");
     var numPw = confirm("Would you like to use Number(s)?");
     var charPW = confirm("Would you like Special Character(s)?");
-    if (! upPw  || lowPw  || numPw || charPW ) {
+    if (!(upPw || lowPw || numPw || charPW)) {
       alert(" Please select at least one character type")
-      
+
     }
 
 
-    
 
-    
+
+
   } else {
     alert("PLEASE SELECT BETWEEN 8 - 128 CHARACTERS!")
-    
+
   }
 
-  var password = generatePassword();
+  password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
